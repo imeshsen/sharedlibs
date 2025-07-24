0540ea9581899761f278238b26ff4bdd348ef47b
Imesh Senanayake <115970061+imeshsen@users.noreply.github.com>
2025-07-24 11:31:16 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -2,6 +2,6 @@ def add(x,y){
   echo "The summation of ${x} and ${y} is ${x+y}"
 }
 
-def mul(x,y){
-  echo "The multiplication of ${x} and ${y} is ${x*y}"
+def mul(Map config = [:]){
+  echo "The multiplication of ${config.x} and ${config.y} is ${config.x*config.y}"
 }
